9fc9dd27c372e558f01e9a3cf53d5cbd171674dd
jwatts41 <wattsjonovan@yahoo.com>
2025-05-01 20:19:41 -0400

    set up server, test route, dependecies and connected MongoDB

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const path = require('path');
+
+dotenv.config(); 
+const app = express();
+
+// Middleware
+app.use(express.json()); 
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// Routes
+app.use('/api', require('./routes/api')); 
+
+// Start server after connecting to MongoDB
+const startServer = async () => {
+  try {
+    // Connect to MongoDB
+    await connectDB(); 
+    console.log('MongoDB connected');
+    
+    // start the server
+    const PORT = process.env.PORT || 3000;  
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error('MongoDB connection failed:', err.message);
+    process.exit(1); 
+  }
+};
+
+
+startServer();
+
---
+++ routes/api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// GET route to test
+router.get('/test', (req, res) => {
+  res.json({ message: 'API test route works!' });
+});
+
+
+module.exports = router;
+ 
